8b7261d924f405e3839746922ef22a0c75e4f496
colllz <ogbonna.collins@gmail.com>
2017-05-18 01:04:50 -0600

    c
    c

--- gen/com/example/irs/R.java
+++ gen/com/example/irs/R.java
@@ -33,25 +33,27 @@ public final class R {
         public static final int irs_logo=0x7f020001;
     }
     public static final class id {
-        public static final int accelerometer_button_id=0x7f0a0015;
-        public static final int action_settings=0x7f0a001a;
+        public static final int accelerometer_button_id=0x7f0a0017;
+        public static final int action_settings=0x7f0a001c;
         public static final int button1=0x7f0a0005;
         public static final int button2=0x7f0a0007;
         public static final int button3=0x7f0a0006;
         public static final int button4=0x7f0a0008;
         public static final int editText1=0x7f0a0003;
-        public static final int editText2=0x7f0a0014;
-        public static final int editText3=0x7f0a0019;
-        public static final int orientation_button_id=0x7f0a0016;
-        public static final int pressure_button_id=0x7f0a0017;
-        public static final int proximity_button_id=0x7f0a0018;
+        public static final int editText2=0x7f0a0016;
+        public static final int editText3=0x7f0a001b;
+        public static final int orientation_button_id=0x7f0a0018;
+        public static final int pressure_button_id=0x7f0a0019;
+        public static final int proximity_button_id=0x7f0a001a;
         public static final int tableRow1=0x7f0a000a;
         public static final int tableRow2=0x7f0a000c;
         public static final int tableRow3=0x7f0a000e;
         public static final int tableRow4=0x7f0a0012;
         public static final int tableRow5=0x7f0a0010;
+        public static final int tableRow6=0x7f0a0014;
         public static final int textView1=0x7f0a0001;
         public static final int textView10=0x7f0a0011;
+        public static final int textView11=0x7f0a0015;
         public static final int textView2=0x7f0a0002;
         public static final int textView3=0x7f0a0000;
         public static final int textView4=0x7f0a0004;
